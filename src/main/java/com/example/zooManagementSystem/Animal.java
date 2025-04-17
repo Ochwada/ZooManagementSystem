e9bf70106ccaa3e7db6b96a530b540b2a4718fdf
ochwada <nakhulolinda@yahoo.com>
2025-04-17 14:32:54 +0200

    started file structure

---
+++ src/main/java/com/example/zooManagementSystem/Animal.java
@@ -0,0 +1,18 @@
+package com.example.zooManagementSystem;
+
+public class Animal {
+
+    /// --------------------  Encapsulation
+    private String name;
+    private int age;
+    private String type;
+
+    /// --------------------  Constructors
+
+    public Animal(){
+
+    }
+
+
+
+}
